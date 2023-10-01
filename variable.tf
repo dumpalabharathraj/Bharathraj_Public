4fe6242a10b5741fb6e1df82274624f73c5359a9
dumpalabharathraj <bharathrajdumpala891@gmail.com>
2023-10-02 01:04:53 +0530

    added

---
+++ variable.tf
@@ -0,0 +1,8 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "vpc_cidr" {}
+variable "public_subnet1_cidr" {}
+variable "public_subnet2_cidr" {}
+variable "public_subnet1_name" {}
+variable "vpc_name" {}
+variable "igw_name" {}
